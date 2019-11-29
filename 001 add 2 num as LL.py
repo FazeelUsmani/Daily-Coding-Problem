e6dc2d1366ab041053b276bc3a4f6a4ee2af9cb0
Fazeel Usmani <fazeelusmani18@gmail.com>
2019-11-29 23:22:35 +0530

    Update 001 add 2 num as LL.py

--- 001 add 2 num as LL.py
+++ 001 add 2 num as LL.py
@@ -1,8 +1,8 @@
-Hi, here's your problem today. This problem was recently asked by Microsoft:
+# Hi, here's your problem today. This problem was recently asked by Microsoft:
 
-You are given two linked-lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+# You are given two linked-lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 
-Example:
+# Example:
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
 Explanation: 342 + 465 = 807.
@@ -33,4 +33,4 @@ while result:
 # 7 0 8
 
 ########################################################################################
-__Solution:__
+#__Solution:__#
