93e5bbd723f6346568a7f82a02fc956d2001a0ca
Fazeel Usmani <fazeelusmani18@gmail.com>
2019-11-29 23:23:02 +0530

    Update 001 add 2 num as LL.py

--- 001 add 2 num as LL.py
+++ 001 add 2 num as LL.py
@@ -3,10 +3,10 @@
 # You are given two linked-lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 
 # Example:
-Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-Output: 7 -> 0 -> 8
-Explanation: 342 + 465 = 807.
-Here is the function signature as a starting point (in Python):
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+# Explanation: 342 + 465 = 807.
+# Here is the function signature as a starting point (in Python):
 
 # Definition for singly-linked list.
 class ListNode(object):
